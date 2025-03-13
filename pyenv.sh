efb7e875eed69f7f12c0ebb83fc30870d5838b62
Sergio H Caliani <shcaliani@gmail.com>
2025-03-13 10:57:09 -0300

    Update pyenv.sh
    Atualizado do Gist

--- pyenv.sh
+++ pyenv.sh
@@ -1,3 +1,26 @@
+🟨🟨 PYENV 
+
+Gerencia o ambiente por diretório para segregar as versões do Python.
+Evita assim alterar o Python do Kernel
+A chamada para cada diretório é direcionada pelo .python-version
+
+||| comandos pyenv
+
+|| versão do python instalada (kernel)
+
+> python --version (num diretório que não tenha o .python-version gerado pelo pyenv
+
+|| brew : instalador do pyenv
+
+> pyenv brew update && brew upgrade                 | atualiza a versão do brew + upgrade
+
+|| pyenv                                            
+
+> pyenv                                             | versão do pyenv e lista de comandos
+> pyenv install --list                              | lista todas as versões disponíveis no pyenv para installar
+> pyenv versions                                    | versão do python instalada no diretório
+> pyenv local 3.11.4 (exemplo)                      | registra no .python-version do diretório a versão a ser utilizada 
+> pyenv install 3.12.2                              | instala no pyenve da home do usuário a versão indicada do python
 
 🚩 brew para utilização do Pyenv
 
@@ -10,9 +33,12 @@ Pyenv : local + versão na pasta
 Pyenv : versions
 Pyenv Install + número da versão
 Pyenv install —List : ver versões existentes
+
 ⭕
 # PyEnv
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+//
