9576424736e622bdde2d07df484b4817225fe10c
Sergio H Caliani <shcaliani@gmail.com>
2025-03-13 12:18:07 -0300

    Update git.sh
    reorganizado

--- git.sh
+++ git.sh
@@ -1,8 +1,6 @@
 🟨🟨 GIT    
 
-'--------------
- --- config ---
- --------------'
+⭕ GIT CONFIG
 
 git config --global user.name "Leonardo Comelli"
 git config --list
@@ -12,6 +10,8 @@ git config --global user.email "eee"
 git config user.name                                    | para ver o conteúdo da configuração
 git config --list                                       | para saber a configuração git
 
+🔶 criar pasta
+
 mkdir nome_pasta                                        | criando uma pasta
 cd nome_pasta
 git init                                                | inicializa o diretório e encherga todas as mudanças e o .git
@@ -19,8 +19,7 @@ cd .git/                                                | possui a head, branche
 
 ## criar o arquivo Readme.md na branche master
 
-
-<> tipos arquivos / lifecycle dos arquivos
+🔶 tipos arquivos / lifecycle dos arquivos
 
 : untracked = não foi visto pelo git
 : unmodified = visto pelo git, mas não modificado
@@ -28,47 +27,22 @@ cd .git/                                                | possui a head, branche
 : staged = pronto para levar qdo criada a nova versão
 
 
-'--------------
- -- comandos --
- --------------'
+⭕ COMANDOS
 
-<> help
+🔶 help
 
 git help add
 git help commit
 git help [qualquer_comando_git]
 
-<> Check
+🔶 check status - diff - log
 
 git status                                              | checa se há arquivos não atualizados
-
-git clone [url] 
-git pull
 
 git diff opção_nome_arquivo                             | ver as mudanças antes commit
 git diff --name-only                                    | exibe somente os nomes dos arquivos alterados
 git diff master                                         | diferença da branch atual para a master
 git diff origin/master                                  | diferença com o repositório master remoto
-
-git add --all
-git commit -m "mensagem"
-git push 
-
-git commit -am "mensagem"                               | adiciona todos e comita direto
-
-
-git rm meu_arquivo.txt                                  | remove arquivo
-git rm -r diretorio                                     | remove diretorio
-
-git checkout -- meu_arquivo.txt                         | Desfaz alteração local enquanto arquivo estiver na staged area
-git reset HEAD meu_arquivo.txt                          | deve ser utilizado quando o arquivo já foi adicionado na staged area
-git reset --soft  _hash_                                | desfaz commit mas deixa o arquivo em staged   - um commit antes
-git reset --mixed _hash_                                | desfaz commit e volta antes staged            - um commit antes
-git reset --hard  _hash_                                | desfaz commit e tudo o que foi feito          - um commit antes
-
-git pull                                                | atualiza diretório local com a nuvem
-
-<> Check Log
 
 git log                                                 | todas as alterações realizadas em um repositório
 git log --graph
@@ -80,11 +54,64 @@ git log --author                                        | mostra somente o autor
 git shortlog                                            | quais autores e número de commits
 git shortlog -sn                                        | quantidade de comits por autor
 
-<> utilizando as hashs
+🔶 retore
 
-git show _hash_                                         | copia a hash de log e inclui na frente de git show
+git restore path_arquivo_exeteção                       | desfaz as modificações voltando o arquivo ao estado anterior
 
-<> subindo todos os arquivos daquele diretório
+🔶 desfaz e reseta
+
+git checkout -- meu_arquivo.txt                         | Desfaz alteração local enquanto arquivo estiver na staged area
+git reset HEAD meu_arquivo.txt                          | deve ser utilizado quando o arquivo já foi adicionado na staged area
+git reset --soft  _hash_                                | desfaz commit mas deixa o arquivo em staged   - um commit antes
+git reset --mixed _hash_                                | desfaz commit e volta antes staged            - um commit antes
+git reset --hard  _hash_                                | desfaz commit e tudo o que foi feito          - um commit antes
+
+🔶 remove arq/dir
+
+git rm meu_arquivo.txt                                  | remove arquivo
+git rm -r diretorio                                     | remove diretorio
+
+🔶 comandos stash
+
+git stash nome_arquivo                                  | um arquivo em wip, então ele não leva para uma nova branch
+git stash list                                          | lista arquivos em modo stash (wip)
+git stash clear                                         | libera arquivos 
+     
+🔶 alias
+
+git config --global alias.sts status
+git sts                                                 | visualiza o status - serve para todos os comandos
+
+🔶 tags gerando versões
+
+git tag -a 1.0.0. -m "mensagem"   
+git push origin master --tags                           | subindo as tags - acessada no github em 'releases'
+git tag                                                 | lista as tags
+
+🔶 apagar branch e tags no diretório remoto
+
+git tag -d 1.0.1                                        | apagando no repositório local
+git push origin :1.0.0                                  | apagando no repositório remoto
+git push origin :nome_branch
+
+
+
+⭕ DESCE : GIT --> LOCAL
+
+🔶 clonar diretório
+
+git clone [url] 
+git pull                                                | atualiza diretório local com a nuvem
+
+🔶 desce tudo 
+
+git pull                                                | atualiza diretório local com a nuvem
+
+
+
+⭕ SOBE : LOCAL --> GIT
+
+🔶 subindo novo diretório 
 
 git remote add origin git@github.com:nome-usuario.git   | ao criar o repositório no git ele fornece o comando e endereço
 git remote                                              | o diretório conectado
@@ -92,66 +119,35 @@ git remote -v                                           | dados da conexão
 git push -u origin master                               | envia tudo para o repositório remoto
 
 git add --all  
-git commit -m"texto versão" 
+git commit -m "novo dir" 
 git push 
-## <sobe do local para a nuvem>
 
-<> subindo apenas um arquivo
+🔶 Inicializando novo diretório local no git
 
-git add nome-arquivo.ext
-git commit -m"texto versão"
-
-<> Tudo ao mesmo tempo
-
-$ git add --all && git commit -m "x" && git push
-
-<> retore
-
-git restore path_arquivo_exeteção                       | desfaz as modificações voltando o arquivo ao estado anterior
-
-
-<> stash
-
-git stash nome_arquivo                                  | um arquivo em wip, então ele não leva para uma nova branch
-git stash list                                          | lista arquivos em modo stash (wip)
-git stash clear                                         | libera arquivos 
-     
-<> alias
-
-git config --global alias.sts status
-git sts                                                 | visualiza o status - serve para todos os comandos
-
-<> tags gerando versões
-
-git tag -a 1.0.0. -m "mensagem"   
-git push origin master --tags                           | subindo as tags - acessada no github em 'releases'
-git tag                                                 | lista as tags
-
-<> revert
-
-git log                                                 | busco o commit que desejo reverter
-git revert _hash_
-
-<> apagar branch e tags no diretório remoto
-
-git tag -d 1.0.1                                        | apagando no repositório local
-git push origin :1.0.0                                  | apagando no repositório remoto
-git push origin :nome_branch
-
-'------------------------------------------
- -- Subir um diretório local para Github --
- ------------------------------------------'
-
-cria diretório no Github                                 | via browser no Github
-git init                                                 | no terminal e diretório a ser feito upload
-git add .                                                | git status mostra arquivos adicionados
-git commit -m "msg"                                      | git status mostra aquivos comitados
+cria diretório no Github                                | via browser no Github
+git init                                                | no terminal e diretório a ser feito upload
+git add .                                               | git status mostra arquivos adicionados
+git commit -m "mensagem"                                | git status mostra aquivos comitados
 git branch -M main    
 
+🔶 sobe só diretório
 
-'--------------
- -- Branches --
- --------------'
+git add --all                                           | git add .
+git commit -m "mensagem"                                | adiciona todos e comita direto
+git push  
+
+🔶 subindo só arquivo
+
+git add nome-arquivo.ext
+git commit -m "mensagem"
+git push
+
+🔶 tudo numa linha
+
+git add --all && git commit -m "x" && git push
+
+
+⭕ BRANCHES
 
 git branch                                              | ver a branch logada
 git checkout                                            | sai da branch
@@ -172,26 +168,36 @@ git branch -d nome_branch                               | exclui branch local se
 git branch -D nome_branch                               | exclui o branch independente de seu status de push ou merge
 git push origin :nome_do_branch_remoto                  | excluir um branch remoto
 
-<> Merge
+
+⭕ UTILIZANDO HASH
+
+🔶 utilizando as hashs
+
+git show _hash_                                         | copia a hash de log e inclui na frente de git show
+
+🔶 revert
+
+git log                                                 | busco o commit que desejo reverter
+git revert _hash_
+
+
+⭕ MERGE
 
 git merge nome_branch_não_master                        | cria um novo commit somando a branch na master
 git commit
 git push
 git log --graph
 
-<> Rebase
+
+⭕ REBASE
 
 git rebase nome_branch_não_master                       | não cria um novo comite, mas alinha a branch à frente da master
 
 
-'----------------------
- -- Pesquisa binária --
- ----------------------'
+⭕ PESQUISA BINÁRIA
 
-
-<> Bisect
+🔶  Bisect
 ## localiza commit que esta gerando um bug ou inconsistência entre uma sequência de commits
-
 
 git bisect start                                        | pequinsa binária
 git bisect bad                                          | Marca commit atual como ruim
@@ -201,22 +207,16 @@ git bisect bad                                          | marca o commit como ru
 git bisect reset                                        | após correção retornar para HEAD
 
 
-'---------------------------------------------
- -- Procedimento Notebook | Gitbash | Linux --
- ---------------------------------------------'
+⭕ PROCEDIMENTO NOTEBOOK | GITBASH | LINUX
+
 [botão direito] 
 Git BASH 
 [abre console]
 
 
-'-----------
- -- Venv --
- -----------'
-Python3 -m Venv_Venv
+⭕⭕⭕
 
-'-----------
- -- Sites --
- -----------'
+🔶 SITES
 
 https://github.com/fernandomayer/git-rautu/blob/master/1_comandos-basicos.md
 https://rogerdudler.github.io/git-guide/index.pt_BR.html
@@ -234,10 +234,7 @@ https://typora.io/
 - Emojis [só copiar que é o próprio código]
 https://getemoji.com/
 
-
-'---------------------------
- -- artigos e referencias --
- ---------------------------'
+🔶 artigos e referências
 
 Google Fire
 https://github.com/google/python-fire
@@ -251,3 +248,5 @@ Comandos diversos
 
 https://gist.github.com/leocomelli/2545add34e4fec21ec16
 Comandos e conflitos
+
+## end
