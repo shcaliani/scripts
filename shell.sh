858c961228da899af8789aaa96725823f3fa52ba
Sergio H Caliani <shcaliani@gmail.com>
2026-03-09 12:33:17 -0300

    Update shell.sh with new command descriptions
    Added additional shell commands and their descriptions.

--- shell.sh
+++ shell.sh
@@ -5,33 +5,46 @@
 ls -la  | lista arquivos diretório
 ls      | lista arquivos
 ll      | lista arquivos do diretório
-df      | Mostra a quantidade de espaço usada no disco rígido.
-top     | Mostra o uso da memória.
+date    | Exibe a data e hora
+history | Lista os últimos comandos usados, muito útil para lembrar também de que comandos foram usados
+who     | Mostra-nos quem está logado no sistema
+sleep 9 | Aguarda o tempo que indicar
+
+
+
+📍Diretórios
 cd      | Acessa uma determinada pasta (diretório)
 mkdir   | Cria um diretório.
 rmdir   | Apaga um diretorio vazio
 rm -r   | Remove diretório não vazio
 rm      | Remove um arquivo
+
+📍Arquivos
 cat     | Abre um arquivo.
 cp      | Copia arquivos
-diff    | Compara o conteúdo de dois arquivos ASCII
 mv      | Move um arquivo
 uniq    | Reporta ou apaga linhas repetidas num arquivo
 wc      | Conta linhas, palavras e mesmo caracteres num arquivo
 fold    | Encurta, ou seja, faz um fold das linhas longas para caberem no dispositivo de output
 head    | Mostra as primeiras linhas de um arquivo, como por exemplo com head -10 a.txt, ou usado como filtro para mostrar apenas os primeiros x resultados de outro comando
+touch   | Criar arquivos
+
+📍Serviços
 ping    | Pingar um determinado host
-jobs    | Permite-nos visualizar jobs em execução, quando corremos uma aplicação em background,
 kill    | Mata um processo, como por exemplo kill -kill 100 ou kill -9 100 ou kill -9 %1
 bg      | Coloca um processo suspenso em background
-date    | Exibe a data e hora
-history | Lista os últimos comandos usados, muito útil para lembrar também de que comandos foram usados
-who     | Mostra-nos quem está logado no sistema
-sleep 9 | Aguarda o tempo que indicar
-touch   | Criar arquivos
 time    | Na frente do comando, monitorar o tempo de execução nas dimensões [time xx.sh ou time ls]
 cut     | Corta o texto conforme definiçao [cut -c2]
+
+📍Processos
 ps      | Lista os processos      [ps | cut -c7]
+df      | Mostra a quantidade de espaço usada no disco rígido.
+jobs    | Permite-nos visualizar jobs em execução, quando corremos uma aplicação em background,
+top     | Processos em andamento (Proc, Mem e outros)
+btop    | Mostra uso da memório: instalar brew intall btop
+vmstat  | check se há swap em disco
+diff    | Compara o conteúdo de dois arquivos ASCII
+
 
 ⭕️ ALIAS
 
